eed712df61a0d7007ceeb018ce7f0ef1c49108b5
dgphus <dphus1507@gmail.com>
2024-06-24 22:20:36 +0700

    fix conflict

--- PRM392_ShopClothes_Repository/obj/Debug/net6.0/PRM392_ShopClothes_Repository.AssemblyInfo.cs
+++ PRM392_ShopClothes_Repository/obj/Debug/net6.0/PRM392_ShopClothes_Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRM392_ShopClothes_Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+193be71a3135d1a7da81e6f0fc23eb1bb4e1d20f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fb783f55f4c9f03d1f50e2e9c2c74436e481ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRM392_ShopClothes_Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRM392_ShopClothes_Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

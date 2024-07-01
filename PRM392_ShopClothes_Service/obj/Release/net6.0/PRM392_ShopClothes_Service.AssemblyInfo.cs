6e5c43bb56b7513977632345be9c1509344fb816
DavidMadLife <quansongngu13@gmail.com>
2024-07-01 22:41:37 +0700

    fix order

--- PRM392_ShopClothes_Service/obj/Release/net6.0/PRM392_ShopClothes_Service.AssemblyInfo.cs
+++ PRM392_ShopClothes_Service/obj/Release/net6.0/PRM392_ShopClothes_Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRM392_ShopClothes_Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdd9f5aa8b6f7a8ff4b04b350d84c492c8be62e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801024633c9a474633ead139b15526d4594a1c5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRM392_ShopClothes_Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRM392_ShopClothes_Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
